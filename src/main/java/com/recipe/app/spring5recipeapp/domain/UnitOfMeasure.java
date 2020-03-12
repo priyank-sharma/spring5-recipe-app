0c2812baf788a07f90f2804f4b79980ab83b1496
Priyank Sharma <priyanksharma@Priyanks-MacBook-Air.local>
2020-03-12 23:44:52 +0530

    Added model classes for recipe, Notes, ingredients and unitofmeasure

---
+++ src/main/java/com/recipe/app/spring5recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,32 @@
+package com.recipe.app.spring5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String description;
+
+	public String getUom() {
+		return description;
+	}
+
+	public void setUom(String uom) {
+		this.description = uom;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
