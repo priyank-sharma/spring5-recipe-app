27fd7a78917327c3d0771fb7ab7e2a5112b60eeb
Priyank Sharma <priyanksharma@Priyanks-MacBook-Air.local>
2020-03-13 00:19:13 +0530

    Add category entity and create many to many relation

---
+++ src/main/java/com/recipe/app/spring5recipeapp/domain/Category.java
@@ -0,0 +1,21 @@
+package com.recipe.app.spring5recipeapp.domain;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String description;
+	
+	@ManyToMany(mappedBy = "categories")
+	Set<Recipe> recipes;
+}
--- src/main/java/com/recipe/app/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/recipe/app/spring5recipeapp/domain/Recipe.java
@@ -9,7 +9,11 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -36,8 +40,14 @@ public class Recipe {
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
 	
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "Recipe")
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	Set<Ingredients> ingredients;
+	
+	@ManyToMany
+	@JoinTable(name = "recipe_category", 
+	joinColumns = @JoinColumn(name = "recipe_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	Set<Category> categories;
 
 	public Set<Ingredients> getIngredients() {
 		return ingredients;
